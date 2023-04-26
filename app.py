f01b3aff30721ac27272f71b5b1885425e127ed4
omveda <35666873+omveda@users.noreply.github.com>
2023-04-25 19:26:17 -0700

    Update app.py

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ consumer = Consumer(conf)
 #topic_name = os.getenv('TOPIC_NAME')
 #print("Topic name is set to: ", topic_name)
 #consumer.subscribe([topic_name])
-consumer.subscribe(['user7-table-changes'])
+consumer.subscribe(['veda-user8-topic'])
 
 def main():
     while True:
